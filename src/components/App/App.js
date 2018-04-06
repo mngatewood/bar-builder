273aea898368ffe7e1ed5d658c9a7b8ef7e4ce15
Michael Gatewood <michael@mngatewood.com>
2018-04-05 19:40:42 -0600

    Adds header and recipes containers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import { Route, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css';
 import { getRecipes } from '../../api/apiCalls/getRecipes';
-import Recipes from '../Recipes/Recipes';
+import { Recipes } from '../Recipes/Recipes';
 import Header from '../Header/Header';
 import { connect } from 'react-redux';
 import { addRecipes } from '../../actions';
---
+++ src/components/Recipes/Recipes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Recipes.css';
+// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+export const Recipes = () => {
+
+  return (
+    <div>
+      <p>Recipes here</p>
+    </div>
+  );
+};
+
+
+// Recipes.propTypes = {
+// };
+
+// export default connect(mapStateToProps)(CardContainer);
