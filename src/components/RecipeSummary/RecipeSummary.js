d3ce46e5fa9d15c390ae236f6d87edba995f7caf
Michael Gatewood <michael@mngatewood.com>
2018-04-07 15:32:39 -0600

    Styles ingredient list

--- src/components/RecipeSummary/RecipeSummary.js
+++ src/components/RecipeSummary/RecipeSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './RecipeSummary.css'
 
 export const RecipeSummary = (recipe) => {
   const { name, 
@@ -33,15 +34,25 @@ export const RecipeSummary = (recipe) => {
     measure14,
     ingredient15,
     measure15 } = recipe;
-  return <div>
+  return <div className="recipe-summary">
     <h4>{name}</h4>
-    <img src={thumbnail} alt="drink image" />
-    <p>{measure1} &nbsp; {ingredient1}</p>
-    <p>{measure2} &nbsp; {ingredient2}</p>
-    <p>{measure3} &nbsp; {ingredient3}</p>
-    <p>{measure4} &nbsp; {ingredient4}</p>
-    <p>{measure5} &nbsp; {ingredient5}</p>
-    <p>{measure6} &nbsp; {ingredient6}</p>
-    <p>{measure7} &nbsp; {ingredient7}</p>
+    <img className="recipe-image" src={thumbnail} alt="cocktail" />
+    <div className="ingredient-list hidden">
+      <p>{measure1}&nbsp;{ingredient1}</p>
+      <p>{measure2}&nbsp;{ingredient2}</p>
+      <p>{measure3}&nbsp;{ingredient3}</p>
+      <p>{measure4}&nbsp;{ingredient4}</p>
+      <p>{measure5}&nbsp;{ingredient5}</p>
+      <p>{measure6}&nbsp;{ingredient6}</p>
+      <p>{measure7}&nbsp;{ingredient7}</p>
+      <p>{measure8}&nbsp;{ingredient8}</p>
+      <p>{measure9}&nbsp;{ingredient9}</p>
+      <p>{measure10}&nbsp;{ingredient10}</p>
+      <p>{measure11}&nbsp;{ingredient11}</p>
+      <p>{measure12}&nbsp;{ingredient12}</p>
+      <p>{measure13}&nbsp;{ingredient13}</p>
+      <p>{measure14}&nbsp;{ingredient14}</p>
+      <p>{measure15}&nbsp;{ingredient15}</p>
+    </div>
   </div>
 }
