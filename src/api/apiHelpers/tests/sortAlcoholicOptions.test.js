4fa8dae561c256297935a74492e79959a440b4e3
Michael Gatewood <michael@mngatewood.com>
2018-04-15 18:54:37 -0600

    Adds tests for all sort filter options functions

---
+++ src/api/apiHelpers/tests/sortAlcoholicOptions.test.js
@@ -0,0 +1,12 @@
+import { sortAlcoholicOptions } from '../sortAlcoholicOptions';
+import * as mock from '../../../mockData/mockData';
+
+describe('sortAlcoholicOptions', () => {
+
+  it('sorts an array of alcoholic options objects', () => {
+    const unsorted = mock.unsortedAlcoholicOptions;
+    expect(unsorted.sort(sortAlcoholicOptions)).
+      toEqual(mock.sortedAlcoholicOptions);
+  });
+  
+})
---
+++ src/api/apiHelpers/tests/sortIngredients.test.js
@@ -0,0 +1,12 @@
+import { sortIngredients } from '../sortIngredients';
+import * as mock from '../../../mockData/mockData';
+
+describe('sortIngredients', () => {
+
+  it('sorts an array of ingredient objects', () => {
+    const unsorted = mock.unsortedIngredientOptions;
+    expect(unsorted.sort(sortIngredients)).
+      toEqual(mock.sortedIngredientOptions);
+  });
+
+})
